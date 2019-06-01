22dc4a0f9ab65fe34bd0781fe604d6cdb67060a2
BuildTools <unconfigured@null.spigotmc.org>
2019-06-01 09:45:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import useInterval from '@use-it/interval';
+import roundTo from 'round-to';
 
-function App() {
+const numberWithCommas = x => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const App = () => {
+  const [cash, setCash] = useState(0);
+  const [clips, setClips] = useState(0);
+  const [totalClips, setTotalClips] = useState(0);
+  const [clipPrice, setClipPrice] = useState(0.1);
+  const [demand, setDemand] = useState(1);
+
+  const makeClip = () => {
+    setClips(clips + 1);
+    setTotalClips(totalClips + 1);
+  };
+
+  const sellClip = () => {
+    setClips(clips - 1);
+    setCash(cash + clipPrice);
+  };
+
+  const calcDemand = () => {
+    setDemand(roundTo(0.1 / clipPrice, 2));
+  };
+
+  const demandToMS = () => {
+    return (1 / demand) * 1000;
+  };
+
+  const increasePrice = () => {
+    setClipPrice(roundTo(clipPrice + 0.01, 2));
+  };
+
+  const decreasePrice = () => {
+    if (clipPrice < 0.02) return;
+    setClipPrice(roundTo(clipPrice - 0.01, 2));
+  };
+
+  useInterval(() => {
+    if (clips > 0) sellClip();
+  }, demandToMS());
+
+  useEffect(calcDemand, [clipPrice]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Total Clips: {numberWithCommas(totalClips)}
+      <br />
+      <br />
+      Unused Clips: {numberWithCommas(clips)}
+      <br />
+      Cash: ${cash.toFixed(2)}
+      <br />
+      Clip Price: ${clipPrice.toFixed(2)}
+      &nbsp;<button onClick={decreasePrice}>&darr;</button>
+      &nbsp;<button onClick={increasePrice}>&uarr;</button>
+      <br />
+      Demand: {(demand * 100).toFixed(0)}% ({demandToMS().toFixed(0)} ms)
+      <br />
+      <br />
+      <button onClick={makeClip}>Make Clip</button>
+    </>
   );
-}
+};
 
 export default App;
