805b1f83a202cc2e58f45e3180a45e7ae2fcf4c2
Connor Younglund <cdy@airmail.cc>
2019-06-02 12:20:09 -0400

    Updates

---
+++ precache-manifest.46626f7fa01bd8cbca3ab3d6845f9903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb652140102a414a80915537f04327a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a658e61ff45ba1de8f4b",
+    "url": "/static/css/main.42010ae9.chunk.css"
+  },
+  {
+    "revision": "6c6f41b38b4e7f7b504e",
+    "url": "/static/js/2.dfc8ccab.chunk.js"
+  },
+  {
+    "revision": "a658e61ff45ba1de8f4b",
+    "url": "/static/js/main.bd2ba15f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
